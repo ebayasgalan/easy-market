d11c308235bef97350cde0c74ee8cdcd65323808
Eric Bayasgalan <ewiz1020@gmail.com>
2019-01-09 18:36:56 -0800

    worked on prisma and graphql yoga

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -4,6 +4,18 @@ const Query = require("./resolvers/Query");
 const db = require("./db");
 
 // Create the graphQL Yoga Server
-function createServer() {}
+function createServer() {
+  return new GraphQLServer({
+    typeDefs: "src/schema.graphql",
+    resolvers: {
+      Mutation,
+      Query
+    },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    },
+    context: req => ({ ...req, db })
+  });
+}
 
 module.exports = createServer;
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,20 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // check to see if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
+    return item;
+  }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+};
 
 module.exports = Mutations;
