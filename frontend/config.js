de43a2bd0d5839024504df30ee532d3875180168
Eric Bayasgalan <ewiz1020@gmail.com>
2019-02-16 17:03:59 -0800

    modified config file

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
-// This is client side config only - don't put anything in here that shouldn't be public!
+// This is client side config only
 export const devEndpoint = `http://localhost:4444`;
 export const prodEndpoint = `https://easy-market-yoga-prod.herokuapp.com/`;
 export const perPage = 4;
