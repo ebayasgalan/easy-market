3c594e793d8e26efae6839ba9c0f641771d3e9fd
Eric Bayasgalan <ewiz1020@gmail.com>
2019-03-02 17:09:12 -0800

    modified a file

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -8,45 +8,48 @@ import CartCount from "./CartCount";
 
 const Nav = () => (
   <User>
-    {({ data: { me } }) => (
-      <NavStyles>
-        <Link href="/items">
-          <a>Shop</a>
-        </Link>
-        {me && (
-          <>
-            <Link href="/sell">
-              <a>Sell</a>
-            </Link>
-            <Link href="/orders">
-              <a>Orders</a>
-            </Link>
-            <Link href="/me">
-              <a>Account</a>
-            </Link>
-            <Signout />
-            <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {toggleCart => (
-                <button onClick={toggleCart}>
-                  My Cart
-                  <CartCount
-                    count={me.cart.reduce(
-                      (tally, cartItem) => tally + cartItem.quantity,
-                      0
-                    )}
-                  />
-                </button>
-              )}
-            </Mutation>
-          </>
-        )}
-        {!me && (
-          <Link href="/signup">
-            <a>Signin</a>
+    {({ data: { me } }) => {
+      console.log("This is inside nav, data.me: ", me);
+      return (
+        <NavStyles>
+          <Link href="/items">
+            <a>Shop</a>
           </Link>
-        )}
-      </NavStyles>
-    )}
+          {me && (
+            <>
+              <Link href="/sell">
+                <a>Sell</a>
+              </Link>
+              <Link href="/orders">
+                <a>Orders</a>
+              </Link>
+              <Link href="/me">
+                <a>Account</a>
+              </Link>
+              <Signout />
+              <Mutation mutation={TOGGLE_CART_MUTATION}>
+                {toggleCart => (
+                  <button onClick={toggleCart}>
+                    My Cart
+                    <CartCount
+                      count={me.cart.reduce(
+                        (tally, cartItem) => tally + cartItem.quantity,
+                        0
+                      )}
+                    />
+                  </button>
+                )}
+              </Mutation>
+            </>
+          )}
+          {!me && (
+            <Link href="/signup">
+              <a>Signin</a>
+            </Link>
+          )}
+        </NavStyles>
+      );
+    }}
   </User>
 );
 
