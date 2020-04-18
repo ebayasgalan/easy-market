b98266f813855c31400e792db8c854ca42aeeac7
Eric Bayasgalan <ewiz1020@gmail.com>
2020-04-17 21:46:56 -0700

    updated some dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,12 +35,12 @@ server.express.use(async (req, res, next) => {
 // start it
 
 server.start(
-  {
-    cors: {
-      credentials: true,
-      origin: process.env.FRONTEND_URL,
-    },
-  },
+  // {
+  //   cors: {
+  //     credentials: true,
+  //     origin: process.env.FRONTEND_URL,
+  //   },
+  // },
   (deets) => {
     console.log(`Server is now running on port http:/localhost:${deets.port}`);
   }
