b10a34a8924953bcfe7db9ad5cc0ac718db77253
ebayasgalan <eric.bayasgalan@gmail.com>
2023-05-06 15:06:42 -0500

    Clean codebase for maintainability

--- frontend/lib/calcTotalPrice.js
+++ frontend/lib/calcTotalPrice.js
@@ -1,6 +1,6 @@
 export default function calcTotalPrice(cart) {
   return cart.reduce((total, cartItem) => {
-    if (!cartItem.product) return total; // products can be deleted, but they could still be in your cart
+    if (!cartItem.product) return total; // products can be deleted, but they could still be in the cart
     return total + cartItem.quantity * cartItem.product.price;
   }, 0);
 }
