aa210972d7f2812d1d5901c0d34c2b5b5d617b8c
ebayasgalan <eric.bayasgalan@gmail.com>
2023-06-01 19:25:04 -0500

    Drop keystone-context from api-route

--- nextjs-app/src/pages/api/graphql.ts
+++ nextjs-app/src/pages/api/graphql.ts
@@ -15,20 +15,20 @@ export default createYoga<{
   res: NextApiResponse;
 }>({
   graphqlEndpoint: '/api/graphql',
-  // schema: keystoneContext.graphql.schema,
-  schema: createSchema({
-    typeDefs: /* GraphQL */ `
-      type Query {
-        greetings: String
-      }
-    `,
-    resolvers: {
-      Query: {
-        greetings: () =>
-          'This is the `greetings` field of the root `Query` type'
-      }
-    }
-  }),
+  schema: keystoneContext.graphql.schema,
+  // schema: createSchema({
+  //   typeDefs: /* GraphQL */ `
+  //     type Query {
+  //       greetings: String
+  //     }
+  //   `,
+  //   resolvers: {
+  //     Query: {
+  //       greetings: () =>
+  //         'This is the `greetings` field of the root `Query` type'
+  //     }
+  //   }
+  // }),
   /*
     `keystoneContext` object doesn't have user's session information.
     You need an authenticated context to CRUD data behind access control.
@@ -36,5 +36,5 @@ export default createYoga<{
     in the request object and gives you a `context` object with session info
     and an elevated sudo context to bypass access control if needed (context.sudo()).
   */
-  context: ({ req, res }) => keystoneContext.withRequest(req, res),
+  // context: ({ req, res }) => keystoneContext.withRequest(req, res),
 });
