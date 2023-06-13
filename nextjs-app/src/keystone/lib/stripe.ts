8b89f11c93a04c31e9f7997e0a798fe8bb84dd2d
ebayasgalan <eric.bayasgalan@gmail.com>
2023-06-12 19:11:41 -0500

    Resolve merge conflict

--- nextjs-app/src/keystone/lib/stripe.ts
+++
@@ -1,7 +0,0 @@
-import Stripe from 'stripe';
-
-const stripeConfig = new Stripe(process.env.STRIPE_SECRET ?? '', {
-  apiVersion: '2022-11-15',
-});
-
-export default stripeConfig;
--- nextjs-app/src/lib/calcTotalPrice.js
+++
@@ -1,6 +0,0 @@
-export default function calcTotalPrice(cart) {
-  return cart.reduce((total, cartItem) => {
-    if (!cartItem.product) return total; // products can be deleted, but they could still be in the cart
-    return total + cartItem.quantity * cartItem.product.price;
-  }, 0);
-}
