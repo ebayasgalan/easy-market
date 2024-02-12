26a884a4a3dc9b07377da2aa174b1925004b6f24
ebayasgalan <eric.bayasgalan@gmail.com>
2024-02-11 18:09:46 -0600

    Rollback and resolve build issue

--- nextjs-app/src/app/api/auth/[...nextauth]/route.ts
+++ nextjs-app/src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
 export const authOptions: AuthOptions = {
